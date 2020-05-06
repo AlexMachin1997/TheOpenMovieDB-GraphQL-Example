5929d7a08618edfcd47598d0a161b5ddee650864
Alex Machin <AlexMachin1997@gmail.com>
2020-05-06 23:00:57 +0100

    feature/credits-unit-tests (#12)

--- utils/resolverUtils/Credits/formatGroup/index.js
+++ utils/resolverUtils/Credits/formatGroup/index.js
@@ -1,70 +1,8 @@
-const generateYear = require("../../../dates/generateYear");
-const replaceKey = require("../../../objects/replaceKey");
-const setValue = require("../../../objects/setValue");
-const mediaTypes = require("./types");
-
-const reorderGroupByReleaseDate = (group) => {
-  group.map((data, index) => {
-    if (data.release_date === "-") {
-      group.splice(index, 1);
-      group.unshift(data);
-    }
-  });
-  return group;
-};
-
-const orderGroupByYear = (group) => {
-  const groupSorted = group.sort((a, b) => {
-    if (a.release_date > b.release_date) {
-      return -1;
-    }
-
-    if (a.release_date < b.release_date) {
-      return 1;
-    }
-
-    return 0;
-  });
-
-  return groupSorted;
-};
-
-const filterGroupByMediaType = (type, group) => {
-  switch (type) {
-    case mediaTypes.ALL: {
-      return group;
-    }
-    case mediaTypes.MOVIE: {
-      return group.filter((data) => data.media_type === "movie");
-    }
-    case mediaTypes.TV: {
-      return group.filter((data) => data.media_type === "tv");
-    }
-    default: {
-      return group;
-    }
-  }
-};
-
-// Any keys which need replacing place them here (Check https://developers.themoviedb.org/3/people/get-person-combined-credits for the schema)
-const replaceKeys = (group) => {
-  group.map((data) => {
-    // Replace first_air_dates (TV credits) with the release_date
-    replaceKey(data, "first_air_date", "release_date");
-
-    // Replace the name with the original_title key (Movies already have this, but for TV it will need to be converted)
-    replaceKey(data, "name", "original_title");
-  });
-
-  return group;
-};
-
-const setValues = (group) => {
-  group.map((data) => {
-    setValue(data, "release_date", generateYear(data.release_date));
-  });
-  return group;
-};
+const reorderGroupByReleaseDate = require("../reorderGroupByReleaseDate");
+const orderGroupByYear = require("../reorderGroupByYear");
+const filterGroupByMediaType = require("../filterGroupByMediaType");
+const replaceKeys = require("../replaceKeys");
+const setValues = require("../setValues");
 
 const formatGroup = (group, mediaType) => {
   // Replace any specified keys
---
+++ utils/resolverUtils/Credits/replaceKeys/index.js
@@ -0,0 +1,16 @@
+const replaceKey = require("../../../objects/replaceKey");
+
+// Any keys which need replacing place them here (Check https://developers.themoviedb.org/3/people/get-person-combined-credits for the schema)
+const replaceKeys = (group) => {
+  group.map((data) => {
+    // Replace first_air_dates (TV credits) with the release_date
+    replaceKey(data, "first_air_date", "release_date");
+
+    // Replace the name with the original_title key (Movies already have this, but for TV it will need to be converted)
+    replaceKey(data, "name", "original_title");
+  });
+
+  return group;
+};
+
+module.exports = replaceKeys;
