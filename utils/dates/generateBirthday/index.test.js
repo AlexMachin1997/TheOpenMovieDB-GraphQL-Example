d85303d79920d375d69570a5408c80253e7440ce
AlexMachin1997 <AlexMachin1997@gmail.com>
2021-12-27 21:28:33 +0000

    fix: Fixed a failing test

--- utils/dates/generateBirthday/index.test.js
+++ utils/dates/generateBirthday/index.test.js
@@ -1,5 +1,18 @@
 const generateBirthday = require('./index');
 
+const moment = require('moment');
+
+jest.mock('moment', () => {
+	const momentParams = {
+		diff: jest.fn(() => 22),
+		format: jest.fn(() => '20/11/1997')
+	};
+
+	const fn = jest.fn(() => momentParams);
+
+	return fn;
+});
+
 describe("generateBirthday unit test's", () => {
 	test('It should return 20/11/1997 (22 years old)', () => {
 		// Arrange and act
