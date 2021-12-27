b24ba9163b71c232279ada73a8b7d7fd80bb9dee
AlexMachin1997 <AlexMachin1997@gmail.com>
2021-12-27 17:16:15 +0000

    chore: Readded the default caching time for responses.

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+// @ts-nocheck
 require('dotenv').config();
 
 const { ApolloServer } = require('apollo-server');
+const { ApolloServerPluginCacheControl } = require('apollo-server-core');
 
 const { schema } = require('./schema');
 
 // Create the ApolloServer instance and pass in the transformed GraphQL schemas (Models) and resolvers (Controllers)
 const server = new ApolloServer({
-	subscriptions: false,
-	schema
+	schema,
+	plugins: [ApolloServerPluginCacheControl({ defaultMaxAge: 3600000 })] // 1 hour
 });
 
 // Start listening to the ApolloServer for GraphQL queries like DiscoverMovies(relaseDate: 2019)
