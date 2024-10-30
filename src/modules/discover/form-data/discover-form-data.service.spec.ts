0a5fa00912ea226c0f494cb1075e2bfb98d83be1
AlexMachin1997 <AlexMachin1997@gmail.com>
2024-10-30 23:21:12 +0000

    chore: Added uncommited file

--- src/modules/discover/form-data/discover-form-data.service.spec.ts
+++ src/modules/discover/form-data/discover-form-data.service.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-useless-template-literals */
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { DiscoverFormDataService } from './discover-form-data.service';
